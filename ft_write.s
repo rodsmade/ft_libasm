db170b479ec88e69c60fbe4676e1e042b1733c48
rodsmade <rodrigogregoldo@gmail.com>
2025-06-01 13:18:56 -0300

    implemented error handling for write syscall

--- ft_write.s
+++ ft_write.s
@@ -1,5 +1,6 @@
 ; ft_write.s
-global ft_write
+global ft_write             ; exports ft_write to the linker
+extern __errno_location     ; imports function to get the pointer to errno
 
 section .text
 ft_write:
@@ -7,6 +8,20 @@ ft_write:
     ;   fd      - a file descriptor to write to
     ;   buffer  - a pointer in memory to (allegedly) relevant data
     ;   n       - the number of chars from buffer that should be written to fd.
+
     mov rax, 1  ; syscall number 1 ( = sys_write)
     syscall
+
+    cmp rax, 0
+    jl handle_error      ; jump to handle_error if rax < 0
+
+    ret
+
+handle_error:
+    neg rax
+
+    call __errno_location
+    mov [rax], rax
+
+    mov rax, -1
     ret
