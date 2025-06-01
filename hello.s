cb8d9909c1de5386c919ef5c33ffb94e030b2869
rodsmade <rodrigogregoldo@gmail.com>
2025-06-01 12:49:04 -0300

    explaining the code line by line

--- hello.s
+++ hello.s
@@ -4,12 +4,12 @@ section .text
 
 ft_hello:
     ; write(1, msg, len)
-    mov     rax, 1          ; syscall number (sys_write)
-    mov     rdi, 1          ; file descriptor (stdout)
-    mov     rsi, msg        ; pointer to message
-    mov     rdx, msg_len    ; message length
-    syscall
-    ret
+    mov     rax, 1          ; syscall number (1 = sys_write)
+    mov     rdi, 1          ; first arg: file descriptor (1 = stdout)
+    mov     rsi, msg        ; second arg: pointer to message string
+    mov     rdx, msg_len    ; third arg: number of bytes to write
+    syscall                 ; make the syscall
+    ret                     ; return to caller (C code)
 
 section .data
 msg:        db "Hello world in Assembly!", 10
