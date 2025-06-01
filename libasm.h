3fc7db9f6afb0423b7c3344bcbdc5d774bbce482
rodsmade <rodrigogregoldo@gmail.com>
2025-06-01 13:19:10 -0300

    updated prototype

--- libasm.h
+++ libasm.h
@@ -2,7 +2,7 @@
 #ifndef LIBASM_H
 #define LIBASM_H
 
-void ft_hello(void);
-void ft_write(int fd, char *buffer, int n);
+void    ft_hello(void);
+int     ft_write(int fd, char *buffer, int n);
 
 #endif
