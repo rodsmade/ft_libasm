ec945b342cec134fcbf4129ad1e22c7733ad30e7
rodsmade <rodrigogregoldo@gmail.com>
2025-06-07 17:15:30 -0300

    fixed: true string initialization

--- test/main.c
+++ test/main.c
@@ -5,7 +5,10 @@
 int main(void) {
     ft_hello();
 
-    char buffer[25] = "First attempt at writing\n";
+    char buffer[26] = "First attempt at writing";
+    buffer[24] = '\n';
+    buffer[25] = '\0';
+
     ft_write(2, buffer, 25);
     printf("Size of string: %lu bytes.\n", ft_strlen(buffer));
 
