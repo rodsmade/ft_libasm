88014218f79ea1bcb2e6494348d365b05b531ebd
rodsmade <rodrigogregoldo@gmail.com>
2025-06-29 19:46:21 -0300

    wee refactor for test usability purposes

--- test/main.c
+++ test/main.c
@@ -5,12 +5,12 @@ int main(void) {
 
     ft_hello();
 
-    run_read_tests();
     run_strcmp_tests();
     run_strcpy_tests();
+    run_strdup_tests();
     run_strlen_tests();
     run_write_tests();
-    run_strdup_tests();
-
+    run_read_tests();
+    
     return 0;
 }
