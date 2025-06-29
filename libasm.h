ab27720ec3cdeab83b806c11e6ddcca533bce27c
rodsmade <rodrigogregoldo@gmail.com>
2025-06-29 16:53:18 -0300

    added missing prototypes

--- libasm.h
+++ libasm.h
@@ -5,7 +5,9 @@
 # include <unistd.h>
 
 void    ft_hello(void);
+ssize_t ft_read(int, void *buf, size_t count);
 int     ft_strcmp(const char *s1, const char *s2);
+char    *ft_strdup(const char *s);
 char    *ft_strcpy(char *dest, const char *src);
 size_t  ft_strlen(const void *string);
 ssize_t ft_write(int fd, const void *buffer, size_t n);
